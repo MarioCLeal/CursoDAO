d53927af35c7888d10eb750900f6c29240b104dc
mario Leal <mariocleal@hotmail.com>
2019-06-22 21:32:03 -0300

    projeto atualizado

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("config.php");
+
+//$sql = new Sql();
+
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(2);
+//echo $root;
+
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega um úsuario usando o login e senha
+//$usuario = new Usuario();
+//$usuario->login("user", "12345");
+
+//echo $usuario;
+
+/* //Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun@");
+$aluno->insert();
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(4);
+
+$usuario->update("professor", "44556688");
+
+echo $usuario;
+
+?>
